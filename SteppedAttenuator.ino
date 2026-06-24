07b8fc9a8c8d1dfd31f99872ccf89f23e896e00b
hoofie2002@yahoo.co.uk <hoofie2002@yahoo.co.uk>
2026-06-24 13:42:53 +0800

    Added slowness to encoder movement

--- SteppedAttenuator.ino
+++ SteppedAttenuator.ino
@@ -17,78 +17,75 @@ const int DB8 = 5;
 const int DB16 = 6;
 const int DB20 = 7;
 
-const int LOW_ATTEN = 0;
-const int HIGH_ATTEN = 51;
-int attenValue = 0;
+const int LOW_ATTEN = 0;    //Minimum Attenuation - pass through
+const int HIGH_ATTEN = 51;  //Maximum Attenuation Possible
+
+const int ENCODER_DEAD_BAND_RANGE = 1; //This stops attenutation value jumping around by putting a small dead band into the encoder
+int attenValue = 0; // Initial Attenuation Value
 
 void setup() {
    //initialize lcd screen
   lcd.init();
   // turn on the backlight
   lcd.backlight();
-  // put your setup code here, to run once:
-
-  //pinMode(DB1, OUTPUT);
-  
-
-
-    // tell the screen to write on the top row
+  // tell the screen to write on the top row
   lcd.setCursor(0,0);
-  // tell the screen to write “hello, from” on the top  row
   lcd.print("Attenuation");
-
+  lcd.setCursor(2, 1);
+  lcd.print("dBm");
 }
 
 long oldPosition  = -999;
 
 void loop() {
 
-    long newPosition = myEnc.read();
+  long rawPosition = myEnc.read();
+
+  long newPosition = rawPosition / 4;
   if (newPosition != oldPosition) {
     if (newPosition > oldPosition) {
       attenValue++;
     } else {
       attenValue--;
     }
+    oldPosition = newPosition;
     if (attenValue > HIGH_ATTEN) attenValue = HIGH_ATTEN;
     if (attenValue < LOW_ATTEN) attenValue = LOW_ATTEN;
-    oldPosition = newPosition;
-    
-  }
-    lcd.setCursor(0, 1);
-    sprintf(buffer,"%02d", attenValue);
-    lcd.print(buffer);
+    }
+  lcd.setCursor(0, 1);
+  sprintf(buffer,"%02d", attenValue);
+  lcd.print(buffer);
 
-    // Action Attend Value on Relays
-    if (attenValue > 20) {
-      digitalWrite(DB20, HIGH);
-    } else {
-      digitalWrite(DB20, LOW);
-    }
-    if ((attenValue & 1) == 1) {
-            digitalWrite(DB1, HIGH);
-    } else {
-      digitalWrite(DB1, LOW);
-    }
-    if ((attenValue & 2) == 2) {
-            digitalWrite(DB2, HIGH);
-    } else {
-      digitalWrite(DB2, LOW);
-    }
-    if ((attenValue & 4) == 4) {
-            digitalWrite(DB4, HIGH);
-    } else {
-      digitalWrite(DB4, LOW);
-    }
-    if ((attenValue & 8) == 8) {
-            digitalWrite(DB8, HIGH);
-    } else {
-      digitalWrite(DB8, LOW);
-    }
-    if ((attenValue & 16) == 16) {
-            digitalWrite(DB16, HIGH);
-    } else {
-      digitalWrite(DB16, LOW);
-    }
+  // Action Attend Value on Relays
+  if (attenValue > 20) {
+    digitalWrite(DB20, HIGH);
+  } else {
+    digitalWrite(DB20, LOW);
+  }
+  if ((attenValue & 1) == 1) {
+    digitalWrite(DB1, HIGH);
+  } else {
+    digitalWrite(DB1, LOW);
+  }
+  if ((attenValue & 2) == 2) {
+    digitalWrite(DB2, HIGH);
+  } else {
+    digitalWrite(DB2, LOW);
+  }
+  if ((attenValue & 4) == 4) {
+    digitalWrite(DB4, HIGH);
+  } else {
+    digitalWrite(DB4, LOW);
+  }
+  if ((attenValue & 8) == 8) {
+    digitalWrite(DB8, HIGH);
+  } else {
+    digitalWrite(DB8, LOW);
+  }
+  if ((attenValue & 16) == 16) {
+    digitalWrite(DB16, HIGH);
+  } else {
+    digitalWrite(DB16, LOW);
+  }
 
 }
